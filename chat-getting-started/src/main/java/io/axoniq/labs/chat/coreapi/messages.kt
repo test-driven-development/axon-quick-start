67a3eab26639f01883062b53bf59fb7f7bb16ce4
WILLIE G PANNELL JR <wil@industriallogic.com>
2020-01-24 21:23:46 -0800

    format for legibility

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/coreapi/messages.kt
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/coreapi/messages.kt
@@ -2,15 +2,31 @@ package io.axoniq.labs.chat.coreapi
 
 import org.axonframework.modelling.command.TargetAggregateIdentifier
 
-data class CreateRoomCommand(@TargetAggregateIdentifier val roomId: String, val name: String)
-data class JoinRoomCommand(val participant: String, @TargetAggregateIdentifier val roomId: String)
-data class PostMessageCommand(val participant: String, @TargetAggregateIdentifier val roomId: String, val message: String)
-data class LeaveRoomCommand(val participant: String, @TargetAggregateIdentifier val roomId: String)
+data class CreateRoomCommand(
+  @TargetAggregateIdentifier val roomId: String, val name: String)
 
-data class RoomCreatedEvent(val roomId: String, val name: String)
-data class ParticipantJoinedRoomEvent(val participant: String, val roomId: String)
-data class MessagePostedEvent(val participant: String, val roomId: String, val message: String)
-data class ParticipantLeftRoomEvent(val participant: String, val roomId: String)
+data class JoinRoomCommand(
+  val participant: String, @TargetAggregateIdentifier val roomId: String)
+
+data class PostMessageCommand(
+  val participant: String,
+  @TargetAggregateIdentifier val roomId:
+  String, val message: String)
+
+data class LeaveRoomCommand(
+  val participant: String, @TargetAggregateIdentifier val roomId: String)
+
+data class RoomCreatedEvent(
+  val roomId: String, val name: String)
+
+data class ParticipantJoinedRoomEvent(
+  val participant: String, val roomId: String)
+
+data class MessagePostedEvent(
+  val participant: String, val roomId: String, val message: String)
+
+data class ParticipantLeftRoomEvent(
+  val participant: String, val roomId: String)
 
 class AllRoomsQuery
 data class RoomParticipantsQuery(val roomId: String)
