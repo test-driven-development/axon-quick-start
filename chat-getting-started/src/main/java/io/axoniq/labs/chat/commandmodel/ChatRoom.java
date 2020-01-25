a1f7ec1efbd8d7ea627e9cba0c56f4696470dd2a
WILLIE G PANNELL JR <wil@industriallogic.com>
2020-01-24 21:53:36 -0800

    implement EventSouringHandler on room created event to implement state changes (separate invariant validation [decision making] in command handler from application of events [state changes] in event sourcing handler)
    make test bar green

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
@@ -3,6 +3,7 @@ package io.axoniq.labs.chat.commandmodel;
 import io.axoniq.labs.chat.coreapi.CreateRoomCommand;
 import io.axoniq.labs.chat.coreapi.RoomCreatedEvent;
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.spring.stereotype.Aggregate;
 
@@ -10,6 +11,8 @@ import static org.axonframework.modelling.command.AggregateLifecycle.apply;
 
 @Aggregate
 public class ChatRoom {
+  public ChatRoom() {
+  }
 
   @AggregateIdentifier
   private String roomId;
@@ -17,5 +20,10 @@ public class ChatRoom {
   @CommandHandler
   public ChatRoom(CreateRoomCommand cmd){
     apply(new RoomCreatedEvent(cmd.getRoomId(), cmd.getName()));
+  }
+
+  @EventSourcingHandler
+  public void on(RoomCreatedEvent event){
+    this.roomId = event.getRoomId();
   }
 }
