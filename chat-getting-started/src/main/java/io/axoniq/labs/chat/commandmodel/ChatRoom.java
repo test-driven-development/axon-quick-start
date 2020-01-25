d0961d8a566eeaf4ba81282e84a0f0893c564408
WILLIE G PANNELL JR <wil@industriallogic.com>
2020-01-24 21:11:24 -0800

    designate aggregate (is now discoverable by spring)

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
@@ -1,5 +1,8 @@
 package io.axoniq.labs.chat.commandmodel;
 
+import org.axonframework.spring.stereotype.Aggregate;
+
+@Aggregate
 public class ChatRoom {
 
   // TODO: This class has just been created to make the test compile. It's missing, well, everything...
