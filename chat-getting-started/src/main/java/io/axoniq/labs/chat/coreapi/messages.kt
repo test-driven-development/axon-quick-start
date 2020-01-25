9612eabf6b289be5766bbaa1c9600ee3f4cde463
WILLIE G PANNELL JR <wil@industriallogic.com>
2020-01-24 21:34:05 -0800

    move events next to commands that fired them

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/coreapi/messages.kt
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/coreapi/messages.kt
@@ -5,25 +5,25 @@ import org.axonframework.modelling.command.TargetAggregateIdentifier
 data class CreateRoomCommand(
   @TargetAggregateIdentifier val roomId: String, val name: String)
 
+data class RoomCreatedEvent(
+  val roomId: String, val name: String)
+
 data class JoinRoomCommand(
   val participant: String, @TargetAggregateIdentifier val roomId: String)
+
+data class ParticipantJoinedRoomEvent(
+  val participant: String, val roomId: String)
 
 data class PostMessageCommand(
   val participant: String,
   @TargetAggregateIdentifier val roomId:
   String, val message: String)
 
-data class LeaveRoomCommand(
-  val participant: String, @TargetAggregateIdentifier val roomId: String)
-
-data class RoomCreatedEvent(
-  val roomId: String, val name: String)
-
-data class ParticipantJoinedRoomEvent(
-  val participant: String, val roomId: String)
-
 data class MessagePostedEvent(
   val participant: String, val roomId: String, val message: String)
+
+data class LeaveRoomCommand(
+  val participant: String, @TargetAggregateIdentifier val roomId: String)
 
 data class ParticipantLeftRoomEvent(
   val participant: String, val roomId: String)
