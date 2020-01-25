fd4711cf68c55469549924156186de82723e09fb
WILLIE G PANNELL JR <wil@industriallogic.com>
2020-01-24 21:45:16 -0800

    implement CreateRoomCommand to apply RoomCreatedEvent (separate invariant validation [decision making] in command handler from application of events [state changes] in event sourcing handler

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
@@ -1,11 +1,21 @@
 package io.axoniq.labs.chat.commandmodel;
 
+import io.axoniq.labs.chat.coreapi.CreateRoomCommand;
+import io.axoniq.labs.chat.coreapi.RoomCreatedEvent;
+import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.spring.stereotype.Aggregate;
+
+import static org.axonframework.modelling.command.AggregateLifecycle.apply;
 
 @Aggregate
 public class ChatRoom {
 
   @AggregateIdentifier
   private String roomId;
+
+  @CommandHandler
+  public ChatRoom(CreateRoomCommand cmd){
+    apply(new RoomCreatedEvent(cmd.getRoomId(), cmd.getName()));
+  }
 }
