b7e8406d98d19be6d792ed1727dc3d6317be2fb9
WILLIE G PANNELL JR <wil@industriallogic.com>
2020-01-24 21:15:06 -0800

    designate aggregate identifier (only mandatory field enforced by axon) to uniquely identify aggregate

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
@@ -1,8 +1,11 @@
 package io.axoniq.labs.chat.commandmodel;
 
+import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.spring.stereotype.Aggregate;
 
 @Aggregate
 public class ChatRoom {
-  
+
+  @AggregateIdentifier
+  private String roomId;
 }
