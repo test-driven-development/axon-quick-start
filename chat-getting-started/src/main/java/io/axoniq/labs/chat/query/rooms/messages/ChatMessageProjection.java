9e89bbdc544bfef87e42817fdcdb27d9c048cf2f
WILLIE G PANNELL JR <wil@industriallogic.com>
2020-01-25 19:23:11 -0800

    implement event handler for chat message projection

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/query/rooms/messages/ChatMessageProjection.java
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/query/rooms/messages/ChatMessageProjection.java
@@ -1,7 +1,12 @@
 package io.axoniq.labs.chat.query.rooms.messages;
 
+import io.axoniq.labs.chat.coreapi.MessagePostedEvent;
+import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.eventhandling.Timestamp;
 import org.axonframework.queryhandling.QueryUpdateEmitter;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
 
 @Component
 public class ChatMessageProjection {
@@ -9,12 +14,21 @@ public class ChatMessageProjection {
   private final QueryUpdateEmitter updateEmitter;
 
   public ChatMessageProjection(
-      ChatMessageRepository repository, QueryUpdateEmitter updateEmitter) {
+      ChatMessageRepository repository,
+      @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+      QueryUpdateEmitter updateEmitter) {
     this.repository = repository;
     this.updateEmitter = updateEmitter;
   }
 
-  // TODO: Create some event handlers that update this model when necessary
+  @EventHandler
+  public void on(MessagePostedEvent event, @Timestamp Instant timestamp) {
+    this.repository.save(new ChatMessage(
+      event.getParticipant(),
+      event.getRoomId(),
+      event.getMessage(),
+      timestamp.toEpochMilli()));
+  }
 
   // TODO: Create the query handler to read data from this model
 
