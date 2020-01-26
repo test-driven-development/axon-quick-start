f8090af4ecfb72470b21ab99fbc887fe2314e959
WILLIE G PANNELL JR <wil@industriallogic.com>
2020-01-25 22:13:20 -0800

    support event handlers for room summary projection

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/query/rooms/summary/RoomSummaryProjection.java
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/query/rooms/summary/RoomSummaryProjection.java
@@ -1,17 +1,37 @@
 package io.axoniq.labs.chat.query.rooms.summary;
 
+import io.axoniq.labs.chat.coreapi.ParticipantJoinedRoomEvent;
+import io.axoniq.labs.chat.coreapi.ParticipantLeftRoomEvent;
+import io.axoniq.labs.chat.coreapi.RoomCreatedEvent;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RoomSummaryProjection {
-
   private final RoomSummaryRepository roomSummaryRepository;
 
+  @Autowired
   public RoomSummaryProjection(RoomSummaryRepository roomSummaryRepository) {
     this.roomSummaryRepository = roomSummaryRepository;
   }
 
-  // TODO: Create some event handlers that update this model when necessary
+  @EventHandler
+  public void on(RoomCreatedEvent event) {
+    this.roomSummaryRepository.save(new RoomSummary(event.getRoomId(), event.getName()));
+  }
+
+  @EventHandler
+  public void on(ParticipantJoinedRoomEvent event) {
+    this.roomSummaryRepository.findById(event.getRoomId())
+      .ifPresent(RoomSummary::addParticipant);
+  }
+
+  @EventHandler
+  public void on(ParticipantLeftRoomEvent event) {
+    this.roomSummaryRepository.findById(event.getRoomId())
+      .ifPresent(RoomSummary::removeParticipant);
+  }
 
   // TODO: Create the query handler to read data from this model
 
