02eeded80c59567cb7384f0af6168c0d88202fcc
WILLIE G PANNELL JR <wil@industriallogic.com>
2020-01-26 20:42:42 -0500

    support all room participants query handler

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/query/rooms/participants/RoomParticipantsProjection.java
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/query/rooms/participants/RoomParticipantsProjection.java
@@ -2,8 +2,12 @@ package io.axoniq.labs.chat.query.rooms.participants;
 
 import io.axoniq.labs.chat.coreapi.ParticipantJoinedRoomEvent;
 import io.axoniq.labs.chat.coreapi.ParticipantLeftRoomEvent;
+import io.axoniq.labs.chat.coreapi.RoomParticipantsQuery;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class RoomParticipantsProjection {
@@ -22,5 +26,10 @@ public class RoomParticipantsProjection {
   @EventHandler
   public void on(ParticipantLeftRoomEvent event) {
     this.repository.deleteByParticipantAndRoomId(event.getRoomId(), event.getParticipant());
+  }
+
+  @QueryHandler
+  public List<RoomParticipant> handle(RoomParticipantsQuery query) {
+    return this.repository.findRoomParticipantsByRoomId(query.getRoomId());
   }
 }
