1df145f5d14372952236c84575e82fb0e4952206
WILLIE G PANNELL JR <wil@industriallogic.com>
2020-01-27 05:07:51 -0500

    invoke room participants query on query gateway from endpoint

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/restapi/QueryController.java
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/restapi/QueryController.java
@@ -1,7 +1,9 @@
 package io.axoniq.labs.chat.restapi;
 
 import io.axoniq.labs.chat.coreapi.AllRoomsQuery;
+import io.axoniq.labs.chat.coreapi.RoomParticipantsQuery;
 import io.axoniq.labs.chat.query.rooms.messages.ChatMessage;
+import io.axoniq.labs.chat.query.rooms.participants.RoomParticipant;
 import io.axoniq.labs.chat.query.rooms.summary.RoomSummary;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
@@ -28,9 +30,10 @@ public class QueryController {
   }
 
   @GetMapping("/rooms/{roomId}/participants")
-  public Future<List<String>> participantsInRoom(@PathVariable String roomId) {
-    // TODO: Send a query for this API call
-    throw new UnsupportedOperationException("Not implemented yet");
+  public Future<List<RoomParticipant>> participantsInRoom(@PathVariable String roomId) {
+    return this.gateway.query(
+        new RoomParticipantsQuery(roomId),
+        ResponseTypes.multipleInstancesOf(RoomParticipant.class));
   }
 
   @GetMapping("/rooms/{roomId}/messages")
